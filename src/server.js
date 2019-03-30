1141ad448219fc277e0f051cdd8c96a10645e353
jch1223 <lux1223@naver.com>
2019-03-30 16:34:02 +0900

    express를 사용하여 rds와 통신하기

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+const express = require('express');
 const password = require('../password.js');
-
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
   host: 'wordnews-database.cbahr4yobiec.us-east-1.rds.amazonaws.com',
   user: 'hee3',
@@ -9,10 +10,10 @@ var connection = mysql.createConnection({
   port: 3306
 });
 
-var http = require('http');
-
-var port = 3000;
+var app = express();
 var ip = '0.0.0.0';
+var port = 3000;
+var headers = defaultCorsHeaders;
 
 function getUser() {
   return new Promise((resolve, reject) => {
@@ -25,30 +26,56 @@ function getUser() {
   });
 }
 
-var server = http.createServer(function(req, res) {
-  var headers = defaultCorsHeaders;
-  var statusCode = 200;
-
-  if (req.method === 'GET') {
-    if (req.url === '/') {
-      console.log('get');
-      getUser()
-        .then(result => {
-          console.log('getUser');
-          res.writeHead(200, headers);
-          res.end(JSON.stringify(result));
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  }
+app.get('/', function(req, res) {
+  console.log('get');
+  getUser()
+    .then(result => {
+      console.log('getUser');
+      res.writeHead(200, headers);
+      res.end(JSON.stringify(result));
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
+app.get('/user/:id', function(req, res) {
+  res.send('received a get request, param : ' + req.params.id);
+});
+
+app.listen(port, ip, function() {
+  console.log('Listening on http://' + ip + ':' + port);
+});
+
+// var http = require('http');
+
+// var port = 3000;
+// var ip = '0.0.0.0';
+
+// var server = http.createServer(function(req, res) {
+//
+//   var statusCode = 200;
+
+//   if (req.method === 'GET') {
+//     if (req.url === '/') {
+//       console.log('get');
+//       getUser()
+//         .then(result => {
+//           console.log('getUser');
+//           res.writeHead(200, headers);
+//           res.end(JSON.stringify(result));
+//         })
+//         .catch(err => {
+//           console.log(err);
+//         });
+//     }
+//   }
+// });
 // eslint-disable-next-line no-console
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
-jjjjjkj;
-module.exports = server;
+//console.log('Listening on http://' + ip + ':' + port);
+//server.listen(port, ip);
+
+//module.exports = server;
 
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
