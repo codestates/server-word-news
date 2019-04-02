4574b1fb356f86361d2ab382511bb8bb21245897
jch1223 <lux1223@naver.com>
2019-04-03 00:28:09 +0900

    add category cookie

--- router/category.js
+++ router/category.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const db = require('../models/index');
 const router = express.Router();
 
 router.get('/', function(req, res) {
   //카테고리 목록을 응답한다
-  res.send('get category ');
+  db.Category.findAll().then(result => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.sendStatus(204);
+    }
+  });
 });
 
+let category;
 router.post('/', function(req, res) {
   //선택된 카테고리id를 저장하고 'Success'라는 문자열을 응답한다.
-  res.send('post categoty');
+  category = req.body.id;
+  res.cookie('category', category).send('cookie : ' + category);
 });
 
 module.exports = router;
