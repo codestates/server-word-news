99279157c1002bc5521a7487b6e8ba2f9ca19828
david718 <hkc7180@gmail.com>
2019-04-06 15:08:21 +0900

    merge success

--- models/Book.js
+++ models/Book.js
@@ -19,11 +19,7 @@ module.exports = function(sequelize, DataTypes) {
         }
       },
       date: {
-<<<<<<< HEAD
-        type: DataTypes.varchar(45),
-=======
         type: DataTypes.STRING(45),
->>>>>>> 78c513c7354b250e0935ac5a21c2ffd79d4998b1
         allowNull: true
       }
     },
