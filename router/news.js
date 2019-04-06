4ed677ddefe9774b17ad140401da374677f36cea
david718 <hkc7180@gmail.com>
2019-04-06 13:07:51 +0900

    news recommends

--- router/news.js
+++ router/news.js
@@ -27,9 +27,18 @@ router.get('/', function(req, res) {
   });
 });
 
-router.get('/recommends', function(req, res) {
+router.post('/recommends', function(req, res) {
   //카테고리에 맞는 기사목록과 ngram을 응답한다
   let wordsArr = req.body;
+  let meaningsArr = [];
+  console.log(wordsArr);
+
+  wordsArr.forEach(async word => {
+    meaningsArr.push(getMeaning(word));
+  });
+  Promise.all(meaningsArr).then(meaningsArr => {
+    res.status(200).json(meaningsArr);
+  });
 });
 
 router.get('/:article_id', function(req, res) {
